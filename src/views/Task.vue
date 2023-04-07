ad8f365e5c11dadf14b6f874e3ba39de0f278c92
Sreesanth <sreesanth@innovaturelabs.com>
2023-04-08 00:39:14 +0530

    feat: getTask by id params

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,14 +1,27 @@
 <template>
-    <div class="task-view"></div>
+    <div class="task-view">
+        <div class="flex flex-col flex-grow itens-start justify-between px-4">
+            {{ task.name }}
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+export default {
+    computed: {
+        ...mapGetters(["getTasks"]),
+
+        task() {
+            return this.getTasks(this.$route.params.id);
+        },
+    },
+};
 </script>
 
 <style>
 .task-view {
-    @apply relative flex flex-row bg-white pin mx-4 m-32 mx-auto py-4 text-left rounded shadow;
+    @apply relative flex flex-row bg-white mx-4 m-32 mx-auto py-4 text-left rounded shadow;
     max-width: 700px;
 }
 </style>
