95badba4c0e5bd0c37068f1e757216f9c62be312
Sreesanth <sreesanth@innovaturelabs.com>
2023-04-07 20:25:37 +0530

    feat: vuex store

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import defaultBoard from './default-board'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        board: defaultBoard
+    },
+
+    mutations: {
+
+    }
+})
