7af485b354c8925d574884c1b73746d11d52a525
Sreesanth <sreesanth@innovaturelabs.com>
2023-04-07 23:44:06 +0530

    fix: board path

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Board from '../views/Board.vue';
 
 const routes = [
 	{
 		path: '/',
-		name: 'home',
-		component: "hey"
+		name: 'board',
+		component: Board
 	},
 ]
 
