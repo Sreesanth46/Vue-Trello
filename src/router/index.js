ff99e6a2c28ee4be10ecd90b78bf01bf026414ae
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:34:32 +0530

    feat: board child component - task

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Board from '../views/Board.vue';
+import Task from '../views/Task.vue';
 
 const routes = [
 	{
 		path: '/',
 		name: 'board',
-		component: Board
+		component: Board,
+		children: [
+			{
+				path: 'task/:id',
+				name: 'task',
+				component: Task,
+			}
+		]
 	},
 ]
 
