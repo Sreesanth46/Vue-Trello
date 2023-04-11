347c4e00c1dea7ca055ab40ab866ff3be482aa16
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:34:32 +0530

    feat: store plugin - save to local storage

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 import Vuex from 'vuex'
 import defaultBoard from './default-board'
+import { saveStatePlugin } from './utils'
+
+const board = JSON.parse(localStorage.getItem('board')) || defaultBoard
 
 const store = new Vuex.Store({
+    plugins: [saveStatePlugin],
+
     state: {
-        board: defaultBoard
+        board
     },
 
     mutations: {
