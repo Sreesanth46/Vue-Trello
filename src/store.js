8e7275008f9a95bf63afb51059982b81e6cb12b6
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:34:32 +0530

    fix: store export

--- src/store.js
+++ src/store.js
@@ -1,10 +1,7 @@
-import Vue from 'vue'
 import Vuex from 'vuex'
 import defaultBoard from './default-board'
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
+const store = new Vuex.Store({
     state: {
         board: defaultBoard
     },
@@ -13,3 +10,5 @@ export default new Vuex.Store({
 
     }
 })
+
+export default store
